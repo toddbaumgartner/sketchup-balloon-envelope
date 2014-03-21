59687a582c2974e32b7d0c59bcaedb8121fb8929
Todd Baumgartner <todd.baumgartner@gmail.com>
2014-03-21 14:02:37 -0400

    Fixed rounding error affecting certain size envelopes, cleaned up gore pattern spreadsheet

--- balloon_envelope/envelope.rb
+++ balloon_envelope/envelope.rb
@@ -11,8 +11,6 @@ Paths=[Dir.home + '/Documents','/tmp',Dir.home + '/My Documents']
 CSVName = 'Envelope.csv'
 
 
-
-
 # First we pull in the standard API hooks.
 require 'csv';
 # Create an entry in the Extension list that loads a script called
@@ -52,46 +50,45 @@ UI.menu("Plugins").add_item("Draw Envelope") {
 }
 
 def draw_panel(sta, width)
-		entities = @entities
-		z = 0
-		@pt2 = @pt1		# Use coordinates from last panel as starting points
-		@pt3 = @pt4
-		# Station marker is in feet, sketchup wants inches (sta * 12)
+  entities = @entities
+  z = 0
+  @pt2 = @pt1		# Use coordinates from last panel as starting points
+  @pt3 = @pt4
+	# Station marker is in feet, sketchup wants inches (sta * 12)
 
-		@pt1 = [12 * sta , width * -0.5, z]		 	# Bottom Left
-		@pt4 = [12 * sta, width * 0.5, z]		# Top Left
+	@pt1 = [12 * sta , width * -0.5, z]		 	# Bottom Left
+  @pt4 = [12 * sta, width * 0.5, z]		# Top Left
 
-		if (width !=  nil)
-			arr = [@pt1, @pt2, @pt3, @pt4].compact.uniq
-			if arr.size > 2
-	    			new_face = entities.add_face arr
-				new_face.pushpull 11
-			end
+  if (width !=  nil)
+		arr = [@pt1, @pt2, @pt3, @pt4].compact.uniq
+      if arr.size > 2
+	   	  new_face = entities.add_face arr
+      	new_face.pushpull 11
+    	end
 		end
-		coordinates = [@pt1] + [0,0,+24]
-		point = Geom::Point3d.new @pt1
-		text = entities.add_text "Station #{sta.round(2)}", point
+	coordinates = [@pt1] + [0,0,+24]
+	point = Geom::Point3d.new @pt1
+  text = entities.add_text "Station #{sta.round(2)}", point
 end
 
-def draw_panel_disc(radius,sta, height, width, num_gores,panel)
+def draw_panel_disc(radius, height, num_gores,panel)
 		entities = @entities
 		circum = radius * 2 * Math::PI	
 		z = 0
 		origin = [0,0,200]
 		@pt2 = @pt1		# Use coordinates from last panel as starting points
 		@pt3 = @pt4
-		# Station marker is in feet, sketchup wants inches (sta * 12)
 
 		pt1 = [0,0,0]
 		pt2 = [0,0,0]
 
 		for gore in 0..num_gores
-			slice = 360 / num_gores		# Split envelope into slices based on number of gores
+			slice = 360.0 / num_gores		# Split envelope into slices based on number of gores- Use float instead of integer
 			degs = slice * gore
 			radians = degs * Math::PI / 180
 			x=origin[0] + radius*Math.cos(radians); 		# Determine location on circumference for panel edges
 			y=origin[1] + radius*Math.sin(radians); 
-			z=origin[2] + (height * 12)
+			z=origin[2] + (height)
 			pt1=[x.round(3),y.round(3),z.round(3)]; 
 
 			if @last_gore[gore] != nil
@@ -99,8 +96,7 @@ def draw_panel_disc(radius,sta, height, width, num_gores,panel)
 				pt4 = @last_gore[gore]
 			end
 
-
-			if (width !=  nil)
+			if (radius !=  0)
 				arr = [pt1, pt2, pt3, pt4].compact.uniq
 
 				if arr.size > 3  && pt2 != [0,0,0] && pt4 != [0,0,0]
@@ -108,10 +104,10 @@ def draw_panel_disc(radius,sta, height, width, num_gores,panel)
 					if @colorgrid[panel] != nil && @colorgrid[panel][gore] != nil
 					     	new_face.material = "##{@color[@colorgrid[panel][gore]]}"
 					else
-					     	new_face.material = "white"
+          new_face.material = "white"
 
 					end
-					new_face.pushpull 0.125
+					new_face.pushpull 0.01
 				end
 			end
 			# Set the stuff for the next go around
@@ -120,6 +116,11 @@ def draw_panel_disc(radius,sta, height, width, num_gores,panel)
 		
 			pt2 = pt1
 
+#                 coordinates = [x, y, z]
+#                 model = Sketchup.active_model
+#                 entities = model.entities
+#                 point = Geom::Point3d.new coordinates
+#                 text = entities.add_text "#{radius.round(2)} #{z.round(2)}", point
 		end
 end
 
@@ -139,84 +140,106 @@ def draw_envelope
   @pt4 = [0, 0, 0]  
   @last_sta = 0
   @last_width = 0
+  @last_rad = 0
   @last_gore = Hash.new
   @colorgrid = Hash.new
   @last_gore[1] = [0,0,0]
 
+
+
+  # With four params, it shows a drop down box for prompts that have
+  # pipe-delimited lists of options. In this case, the Gender prompt
+  # is a drop down instead of a text box.
+#  prompts = ["What is your Name?", "What is your Age?", "Panel Shape"]
+#  defaults = ["Enter name", "", "Male"]
+#  list = ["", "", "Natural Boundries|Fabric Width"]
+  prompts = ["Panel Shape"]
+  defaults = ["Natural Boundries"]
+  list = ["Natural Boundries|Fabric Width"]
+  input = UI.inputbox(prompts, defaults, list, "What are we doing today?")
+  if input != nil
+    shape = input[0]
+  end
+
+  model.rendering_options["EdgeDisplayMode"]=1    # 0 to hide edges
+
   @EnvelopeCSV = UI.openpanel("Open Envelope CSV File", "c:/", "*.csv")
 
-
   # Parse the output CSV from the Deering_Gore_pattern.xlsm file
-#  Paths.each do |path|
-#  	try = path + '/' + CSVName
-#  	if File.exists?(try)
-#		@EnvelopeCSV = try
-#		puts "Using: #{@EnvelopeCSV}"
-#  	end
-#  end
+  #  Paths.each do |path|
+  #  	try = path + '/' + CSVName
+  #  	if File.exists?(try)
+  #		@EnvelopeCSV = try
+  #		puts "Using: #{@EnvelopeCSV}"
+  #  	end
+  #  end
 
 
   if (@EnvelopeCSV) != nil
   	CSV.foreach(@EnvelopeCSV) do |row|	# First time around, get the color grid
-
-		for idx in 0..row.size do 
-			if row[idx] ==  "Color Chart Values"
-				row.shift idx + 1
-				@colorgrid[row.shift.to_i]=row	# Populate Color Grid with contents of line
-			end
-		end
-	end
+		  for idx in 0..row.size do 
+			  if row[idx] ==  "Color Chart Values"
+				  row.shift idx + 1
+				  @colorgrid[row.shift.to_i]=row	# Populate Color Grid with contents of line
+			  end
+		  end
+	  end
   	CSV.foreach(@EnvelopeCSV) do |row|
-		sta = row[2].to_f
-		radius = row[3].to_f
-		circumference = row[5].to_f
-		width = row[6].to_f
-		height = row[37].to_f
-		if (sta != 0 && radius != 0 && width != 0) 
-			if width > 0 
-				num_gores = (circumference / width).round
-				ratio = sta / width
-			end
-			
-			puts "Station #{sta.round(2)}ft Radius #{radius.round(2)}in Width #{width.round(2)}in Height #{height.round(2)}in Gores: #{num_gores}"
-		
-			centerpoint = Geom::Point3d.new(0,0,height * 12)
-			vector = Geom::Vector3d.new 0,0,1
-		 	vector2 = vector.normalize!
+		  sta = row[2].to_f
+		  radius = row[3].to_f
+		  circumference = row[5].to_f
+		  width = row[6].to_f
+		  height = row[37].to_f
+		  height = height * 12    # Lets use inches
 			if radius == 0 
-				radius = 1
+        radius = 0.01 
 			else
-			   radius = radius * 12
+			 radius = radius * 12   # Same here
 			end
-#			panel = panel + 1
-#			draw_panel_disc(radius, sta, height, width, num_gores, panel )
+	
+		  if (sta != 0 && radius != 0 && width != 0) 
+			  if width > 0 
+				  num_gores = (circumference / width).round
+			  end
+			
+		
+			  centerpoint = Geom::Point3d.new(0,0,height)
+			  vector = Geom::Vector3d.new 0,0,1
+		 	  vector2 = vector.normalize!
+		    # Fork in the road here
+		    # 
+		    if shape == "Natural Boundries"
+			    panel = panel + 1
+			    draw_panel_disc(radius, height,  num_gores, panel )
+		    else
+         	# Figure where fabric edges would be
+       		@fabsize = 60         # Width of fabric 
+      		@mid = (@last_sta * 12).to_i / @fabsize     # Find last panel edge based on last station marker
+	       	@curmid = (sta * 12).to_i / @fabsize        # Find current panel edge based on current station marker
+          if (@last_sta != 0 ) 
+         		@lastradratio = @last_rad / @last_sta       # Theres probably a better way to do this
+          end
+       		@radratio = radius / sta                 # Theres probably a better way to do this
+       		@heightratio = height / sta           # Ditto
+          @avgradratio = (@lastradratio + @radratio)  / 2.0
+       		@radius = @mid * @fabsize / 12 * @avgradratio 
+       		@height = @mid * @fabsize * @heightratio / 12   
+         		if (@curmid != @mid)    
+              	panel = panel + 1
+                draw_panel_disc(@radius, @height, num_gores, panel)
+                #
 
-                # Figure where fabric edges would be
-#
-               @fabsize = 60
-#
-               @mid = (@last_sta * 12).to_i / @fabsize
-               @curmid = (sta * 12).to_i / @fabsize
-               @widratio = width / sta
-               @heightratio = height / sta
-               @width = @mid * @fabsize * @widratio / 12       
-               @height = @mid * @fabsize * @heightratio / 12   
-               #puts "Last Sta: #{@last_sta} LastMid: #{@mid * @fabsize / 12} Next: #{sta} CurMid: #{@curmid * @fabsize /12} Width: #{@width} Ratio: #{@ratio.round(2)}"
-#       
-               if (@curmid != @mid)    
-                       panel = panel + 1
-                       draw_panel_disc(radius, @curmid, @height, @width, num_gores, panel)
-               end
-#
 
+                puts ">Station #{@mid * @fabsize / 12}, Radius #{@radius.round(2)} Width #{@width.round(2)} Height #{@height}, #{num_gores}, #{panel} RadRatio #{@radratio.round(2)} LastRadRatio #{@lastradratio.round(2)} "
+         		end
+		    end
 
-
-
-
-			@last_sta = sta
-			@last_width = width
-			@last_height = height
-		end
+        puts "Station #{sta.round(2)} Radius #{radius.round(2)} Width #{width.round(2)} Height #{height.round(2)} Gores #{num_gores} RadRatio #{@radratio.round(2)} "
+			  @last_sta = sta
+			  @last_width = width
+			  @last_height = height
+			  @last_rad = radius
+		  end
 	  end
   end
 end
